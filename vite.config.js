3917f65866c7f651324d036d198874dde3a15e6e
zhangliu <2351186624@qq.com>
2021-09-16 14:16:43 +0800

    优化

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import vue from "@vitejs/plugin-vue";
 import styleImport from "vite-plugin-style-import";
 import { defineConfig } from "vite";  // 帮手函数，这样不用 jsdoc 注解也可以获取类型提示
-import { resolve } from "path"; // 主要用于alias文件路径别名
-
 export default defineConfig({
   plugins: [
     vue(),
